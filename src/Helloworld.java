54156abf9dd8cef2d4c9c95d3a2279acbe7fae98
Dan Sullivan <dansullivan@dan-sullivans-macbook-pro-2.local>
2013-01-26 13:58:20 -0500

    initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,15 @@
+
+public class Helloworld {
+
+	public void hello() {
+		System.out.println("hello, world");
+	}
+	/**
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		Helloworld h = new Helloworld();
+		h.hello();
+	}
+
+}
